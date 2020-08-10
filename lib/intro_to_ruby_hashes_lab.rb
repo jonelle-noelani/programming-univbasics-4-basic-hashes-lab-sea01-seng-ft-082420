c2c443379d2a715d3a3060f020675fa088b818e0
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-10 01:13:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -9,9 +9,9 @@ def my_hash
 end
 
 # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-# def pioneer
-#     pioneer = {:name => “Grace Hopper”}
-# end
+def pioneer
+    pioneer = {:name => “Grace Hopper”}
+end
 
  # return a hash with a key :id assigned to the provided number
 # def id_hash_generator(number)
