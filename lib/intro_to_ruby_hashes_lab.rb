47ba52c913f2f0de253ed4ebe0cb3902b02459af
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-10 00:58:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -10,13 +10,13 @@ end
 #   my_hash
 # end
 
-def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  pioneer[:name] => “Grace Hopper”
-end
+# def pioneer
+#   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+#   pioneer[:name] => “Grace Hopper”
+# end
 
 
-def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
-  id_hash_generator[:id] = integer
-end
+# def id_hash_generator(number)
+#   # return a hash with a key :id assigned to the provided number
+#   id_hash_generator[:id] = integer
+# end
